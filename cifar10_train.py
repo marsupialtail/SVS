13772853d9cdc10b33202889f4dcc344a707b0f7
Ziheng Wang <ziheng.wang@cerebras.net>
2019-03-30 20:20:15 -0100

    Add files via upload

--- cifar10_train.py
+++ cifar10_train.py
@@ -57,71 +57,87 @@ tf.app.flags.DEFINE_integer('log_frequency', 10,
 
 
 def train():
-  """Train CIFAR-10 for a number of steps."""
-  with tf.Graph().as_default():
-    global_step = tf.train.get_or_create_global_step()
+    """Train CIFAR-10 for a number of steps."""
+    with tf.Graph().as_default():
+        global_step = tf.train.get_or_create_global_step()
 
-    # Get images and labels for CIFAR-10.
-    # Force input pipeline to CPU:0 to avoid operations sometimes ending up on
-    # GPU and resulting in a slow down.
-    with tf.device('/cpu:0'):
-      images, labels = cifar10.distorted_inputs()
+        # Get images and labels for CIFAR-10.
+        # Force input pipeline to CPU:0 to avoid operations sometimes ending up on
+        # GPU and resulting in a slow down.
+        with tf.device('/cpu:0'):
+            images, labels = cifar10.distorted_inputs()
 
-    # Build a Graph that computes the logits predictions from the
-    # inference model.
-    logits = cifar10.inference(images)
+        images = tf.transpose(images,[0,3,1,2])
 
-    # Calculate loss.
-    loss = cifar10.loss(logits, labels)
 
-    # Build a Graph that trains the model with one batch of examples and
-    # updates the model parameters.
-    train_op = cifar10.train(loss, global_step)
+        # Build a Graph that computes the logits predictions from the
+        # inference model.
+        logits = cifar10.inference(images)
 
-    class _LoggerHook(tf.train.SessionRunHook):
-      """Logs loss and runtime."""
+        # Calculate loss.
+        loss = cifar10.loss(logits, labels)
 
-      def begin(self):
-        self._step = -1
-        self._start_time = time.time()
+        # Build a Graph that trains the model with one batch of examples and
+        # updates the model parameters.
+        train_op = cifar10.train(loss, global_step)
 
-      def before_run(self, run_context):
-        self._step += 1
-        return tf.train.SessionRunArgs(loss)  # Asks for loss value.
+        class _LoggerHook(tf.train.SessionRunHook):
+            """Logs loss and runtime."""
 
-      def after_run(self, run_context, run_values):
-        if self._step % FLAGS.log_frequency == 0:
-          current_time = time.time()
-          duration = current_time - self._start_time
-          self._start_time = current_time
+            def begin(self):
+                self._step = -1
+                self._start_time = time.time()
 
-          loss_value = run_values.results
-          examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
-          sec_per_batch = float(duration / FLAGS.log_frequency)
+            def before_run(self, run_context):
+                self._step += 1
+                return tf.train.SessionRunArgs(loss)  # Asks for loss value.
 
-          format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                        'sec/batch)')
-          print (format_str % (datetime.now(), self._step, loss_value,
-                               examples_per_sec, sec_per_batch))
+            def after_run(self, run_context, run_values):
+                if self._step % FLAGS.log_frequency == 0:
+                    current_time = time.time()
+                    duration = current_time - self._start_time
+                    self._start_time = current_time
 
-    with tf.train.MonitoredTrainingSession(
-        checkpoint_dir=FLAGS.train_dir,
-        hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
-               tf.train.NanTensorHook(loss),
-               _LoggerHook()],
-        config=tf.ConfigProto(
-            log_device_placement=FLAGS.log_device_placement)) as mon_sess:
-      while not mon_sess.should_stop():
-        mon_sess.run(train_op)
+                    loss_value = run_values.results
+                    examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
+                    sec_per_batch = float(duration / FLAGS.log_frequency)
+
+                    format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                                  'sec/batch)')
+                    print(format_str % (datetime.now(), self._step, loss_value,
+                                        examples_per_sec, sec_per_batch))
+
+        with tf.contrib.tfprof.ProfileContext('profiling_stuff') as pctx:
+            run_metadata = tf.RunMetadata()
+            with tf.train.MonitoredTrainingSession(
+                    checkpoint_dir=FLAGS.train_dir,
+                    hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
+                           tf.train.NanTensorHook(loss),
+                           _LoggerHook()],
+                    config=tf.ConfigProto(
+                        log_device_placement=FLAGS.log_device_placement)) as mon_sess:
+                while not mon_sess.should_stop():
+                    mon_sess.run(train_op,options=tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE),
+                                 run_metadata=run_metadata)
+        # with tf.contrib.tfprof.ProfileContext('profiling_stuff') as pctx:
+        # with tf.train.MonitoredTrainingSession(
+        #         checkpoint_dir=FLAGS.train_dir,
+        #         hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
+        #                tf.train.NanTensorHook(loss),
+        #                _LoggerHook()],
+        #         config=tf.ConfigProto(
+        #             log_device_placement=FLAGS.log_device_placement)) as mon_sess:
+        #     while not mon_sess.should_stop():
+        #         mon_sess.run(train_op)
 
 
 def main(argv=None):  # pylint: disable=unused-argument
-  cifar10.maybe_download_and_extract()
-  if tf.gfile.Exists(FLAGS.train_dir):
-    tf.gfile.DeleteRecursively(FLAGS.train_dir)
-  tf.gfile.MakeDirs(FLAGS.train_dir)
-  train()
+    cifar10.maybe_download_and_extract()
+    if tf.gfile.Exists(FLAGS.train_dir):
+        tf.gfile.DeleteRecursively(FLAGS.train_dir)
+    tf.gfile.MakeDirs(FLAGS.train_dir)
+    train()
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
