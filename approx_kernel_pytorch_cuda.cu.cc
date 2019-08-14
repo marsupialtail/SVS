bee8c1ccb745fd1c2b49292b9277a4081ccaa2d3
Ziheng Wang <zwang@asapp.com>
2019-08-14 14:13:50 -0700

    fixed some bugs

--- approx_kernel_pytorch_cuda.cu.cc
+++ approx_kernel_pytorch_cuda.cu.cc
@@ -16,7 +16,7 @@
 #define OFF 3
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+__inline__ void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
    if (code != cudaSuccess)
    {
